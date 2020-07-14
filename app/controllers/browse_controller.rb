87acfee9afa1497b6eed2319b50f514b31685d6b
Yan Santos <yan17souza@hotmail.com>
2020-07-14 17:17:20 -0300

    browse view with all user created

---
+++ app/controllers/browse_controller.rb
@@ -0,0 +1,15 @@
+class BrowseController < ApplicationController
+
+  def browse
+    @users = Account.all
+  end
+
+  def approve
+    #user swipes right
+  end
+
+  def decline
+    #user swipes left
+  end
+
+end
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,11 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+Account.create([
+  {first_name: "Sarah", last_name: "Karlin", username: "Sarah" , email: "sarah@example.com", password:"121212"},
+  {first_name: "Emily", last_name: "Justine", username: "Emily", email: "emily@example.com", password:"121212"},
+  {first_name: "Roger", last_name: "Jones", username: "Roger", email: "roger@example.com", password:"121212"},
+  {first_name: "Caroline", last_name: "Molas", username: "Caroline", email: "caroline@example.com", password:"121212"},
+  {first_name: "Poul", last_name: "Smith", username:  "Poul", email: "poul@example.com", password:"121212"},
+])
