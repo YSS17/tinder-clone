28864036b4695ab3a936479fc6dc06cdf96e2e41
Yan Souza <yan17souza@hotmail.com>
2020-07-31 15:39:54 -0300

    adding a my matches pages to the application

--- app/models/account.rb
+++ app/models/account.rb
@@ -7,8 +7,8 @@ class Account < ApplicationRecord
   has_many_attached :images
 
   def matches
-    liked_account_ids = like.where(account_id: self.id).map(&:liked_account_id)
-    likes_me_account_ids = like.where(liked_account_id: self.id).map(&:account_id)
+    liked_account_ids = Like.where(account_id: self.id).map(&:liked_account_id)
+    likes_me_account_ids = Like.where(liked_account_id: self.id).map(&:account_id)
 
     matched_ids = liked_account_ids.select{|id| likes_me_account_ids.include?(id)}
 
