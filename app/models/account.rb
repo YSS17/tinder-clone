7bbeda4609bb92cf679afc3eda473f752705aeb0
Yan Souza <yan17souza@hotmail.com>
2020-08-03 18:25:58 -0300

    solving bug in my matches img

--- app/models/account.rb
+++ app/models/account.rb
@@ -7,11 +7,13 @@ class Account < ApplicationRecord
   has_many_attached :images
 
   def matches
+    # find accounts that current account liked
     liked_account_ids = Like.where(account_id: self.id).map(&:liked_account_id)
+    # find accounts that liked current account
     likes_me_account_ids = Like.where(liked_account_id: self.id).map(&:account_id)
-
+    # get paired accounts ids that liked each other
     matched_ids = liked_account_ids.select{|id| likes_me_account_ids.include?(id)}
-
+    # find matched accounts
     Account.where(id: matched_ids)
   end
 end
