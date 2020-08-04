564bd61e4360f7e42dd6f288e42a38bd79965077
Yan Souza <yan17souza@hotmail.com>
2020-08-04 16:25:42 -0300

    added JS to my matches and fucntion click

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -11,6 +11,10 @@ require("bootstrap")
 require("../includes/slides.js")
 require("@fortawesome/fontawesome-free");
 
+require("../includes/matches.js")
+
+
+
 // Uncomment to copy all static images under ../images to the output folder and reference
 // them with the image_pack_tag helper in views (e.g <%= image_pack_tag 'rails.png' %>)
 // or the `imagePath` JavaScript helper below.
